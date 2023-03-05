677d71ff99a23b2f00fa72afb25f10c0b8c13aa8
Noman1313 <tasnimulhasannoman@gmail.com>
2023-03-05 22:19:08 +0600

    null

---
+++ null-vs-undefines.js
@@ -0,0 +1,19 @@
+// let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add(num1,num2) {
+//     console.log(num2);
+// }
+
+
+// const result = {name:"abul", phone: 01533545145 };
+// console.log(result.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [13, 13, 14];
+console.log(ages[6]);
