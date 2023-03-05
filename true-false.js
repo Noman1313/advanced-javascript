23de53b6c5cf4d8d07d4632db35a893734b26fed
Noman1313 <tasnimulhasannoman@gmail.com>
2023-03-05 13:30:39 +0600

    true/false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// truthy
+// "0", " ", []
+
+
+// const name = "0";
+let name = 11;
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}   
